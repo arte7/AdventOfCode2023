a8b3515a991050ad9e4d5e533ad2c5f2eb271281
Pia Bregulla <pia.bregulla@xing.com>
2023-12-05 01:10:24 +0100

    far left lookup working

--- AOC/day3.rb
+++ AOC/day3.rb
@@ -23,17 +23,17 @@ signs = []
 i = 0
 j = 0
 
-NUMS = /[\d]/
+NUMS = /[\d+]/
 CHARS = /[!@#$%^&*_+{}\[\]:;<>,?~\\\/\-]/
 input.each_line do |line|
   line = line.strip!
   matrix << line
 end
-col = matrix.length - 1
+rows = matrix.length
 
-find_chars = for j in 0..col do
-  len = matrix[j].length - 1
-  for i in 0..len do
+find_chars = for j in 0...rows do
+  len = matrix[j].length
+  for i in 0...len do
     # p matrix[j][i] # for debugging
     unless matrix[j][i].match(CHARS).nil?
       signs << [j, i]
@@ -41,14 +41,37 @@ find_chars = for j in 0..col do
   end
 end
 
-find_adjacent = for i in 0..(signs.length - 1) do
+find_adjacent = for i in 0...(signs.length) do
   p "sings in find_adjacent #{signs[i]}"
-  c = signs[i][0] # columns || j
-  e = signs[i][1] # elements || i
-  if c > 0
-    c -
+  r = signs[i][0] # rows aka j
+  c = signs[i][1] # columns aka i
+  if r > 0
+    row_up = matrix[r - 1][c]
+    if row_up.match(NUMS).nil?
+      p "left range #{@far_left_range}"
+      # p @far_left_range.scan(NUMS).join.to_i
+      @far_left_range = if c >= 4 && matrix[r - 1][c-1].scan(NUMS).nil?
+        s = c-4
+        far_left_range = matrix[r-1][s...c]
+        p "first loop left #{far_left_range}"
+      elsif c >= 3
+        s = c-3
+        far_left_range = matrix[r-1][s...c]
+        p "second loop left #{far_left_range}"
+      elsif c >= 2
+        s = c-2
+        far_left_range = matrix[r-1][s...c]
+        p "third loop left #{far_left_range}"
+      end
+    end
   else
+    p "weiß nit"
   end
+
+  # else
+  #   start_left_range = matrix[r-1][c..c+3]
+  #   p "third loop left #{start_left_range}"
+  # end
 end
 
 
